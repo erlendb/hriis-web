b6f22b15d5a6af5fcd7ff162fede9cd883fb261f
Erlend Blomseth <erlend@elendig.no>
2021-08-13 22:07:12 +0200

    Setup smpty components and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./components/header/header";
+import About from "./components/about/about";
+import Contact from "./components/contact/contact";
+import Cabins from "./components/cabins/cabins";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hei</h1>
-      <p>Dette er en test</p>
+    <div>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route path='/'>
+            <About />
+          </Route>
+          <Route path='/kontakt'>
+            <Contact />
+          </Route>
+          <Route path ='/hytter'>
+            <Cabins />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
